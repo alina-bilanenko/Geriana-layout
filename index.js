1494e75015a25844a7dbb6be85cc958a90f27968
alina-bilanenko <Linyska007@gmail.com>
2018-07-18 21:41:05 +0300

    how-it-work

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-//top scroll
-$(document).ready(function(){
-  let viewport = $( window ).width();
-$(window).scroll(function() {
-    if (($(this).scrollTop()>200 && viewport > 767) || ($(this).scrollTop()>600 && viewport <= 767)){
-      $('#topHeader').css({display: 'flex', position: 'fixed', top: '0px'});
-    } else {
-      $('#topHeader').css({display: 'none'});
-    }
-  })
-});
-
+// //top scroll
+// $(document).ready(function(){
+//   let viewport = $( window ).width();
+// $(window).scroll(function() {
+//     if (($(this).scrollTop()>200 && viewport > 767) || ($(this).scrollTop()>600 && viewport <= 767)){
+//       $('#topHeader').css({display: 'flex', position: 'fixed', top: '0px'});
+//     } else {
+//       $('#topHeader').css({display: 'none'});
+//     }
+//   })
+// });
+//

d2f6e6774a67260885e233098fe4e507964aa4ca
alina-bilanenko <Linyska007@gmail.com>
2018-07-23 16:52:30 +0300

    media queries

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let viewport = document.documentElement.clientWidth;
 
   window.onscroll = function() {
 
+    let viewport = document.documentElement.clientWidth;
+
+    // if (viewport < 769) return;
+
     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    if ((scrolled > 200) || (scrolled > 400 && viewport <= 767)){
+    if ((viewport >= 769) && ((scrolled > 200 && viewport >= 992) || (scrolled > 100 && viewport < 992))){
 
       Object.assign(document.querySelector('#topHeader').style, {display: 'flex'});
-      Object.assign(document.querySelector('.button-call').style, {display: 'block'});
+      Object.assign(document.querySelector('#buttonCallHide').style, {display: 'block'});
 
     } else {
       Object.assign(document.querySelector('#topHeader').style, {display: 'none'});
-      Object.assign(document.querySelector('.button-call').style, {display: 'none'});
+      Object.assign(document.querySelector('#buttonCallHide').style, {display: 'none'});
     }
   }
 });

df46ca903eee135a432e1a376fb567863675b833
alina-bilanenko <Linyska007@gmail.com>
2018-07-24 00:12:34 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
+import "./css/index.css";
+
 document.addEventListener("DOMContentLoaded", function() {
 
   window.onscroll = function() {
 
     let viewport = document.documentElement.clientWidth;
-
-    // if (viewport < 769) return;
-
     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    if ((viewport >= 769) && ((scrolled > 200 && viewport >= 992) || (scrolled > 100 && viewport < 992))){
 
+    if ((viewport >= 768) && ((scrolled > 200 && viewport >= 992) || (scrolled > 100 && viewport < 992))){
       Object.assign(document.querySelector('#topHeader').style, {display: 'flex'});
       Object.assign(document.querySelector('#buttonCallHide').style, {display: 'block'});
-
-    } else {
+    } else
+    {
       Object.assign(document.querySelector('#topHeader').style, {display: 'none'});
       Object.assign(document.querySelector('#buttonCallHide').style, {display: 'none'});
     }
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+
+            }
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|ttf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'logo/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader'
+        }],
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {} ),
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[name].[chunkhash].css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+  ]
+};
